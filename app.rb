28d85072516a3231416421e06d320096df3c648c
César Salazar <i.am@cesarsalzar.mx>
2011-05-01 03:00:42 -0500

    Initial commit

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/content_for'
+require 'sinatra/reloader' if development?
+require 'haml'
+require 'sass'
+require 'rdiscount'
+
+get '/' do
+  haml :index
+end
+
+get '/stylesheets/*' do
+  content_type 'text/css'
+  sass '../styles/'.concat(params[:splat].join.chomp('.css')).to_sym
+end
+
+get '/:location' do
+  haml params[:location].to_sym
+end
